b9df56b63dea264808caae29940d71ab0a4cf40d
dolong <dolong@gmail.com>
2022-03-03 22:36:33 -0500

    css

--- components/elements/News.js
+++ components/elements/News.js
@@ -85,26 +85,7 @@ const News = () => {
                                                     <div className="news_popup_informations">
                                                         <div className="text">
                                                             <p>
-                                                                Kura is a
-                                                                leading web
-                                                                design agency
-                                                                with an
-                                                                award-winning
-                                                                design team that
-                                                                creates
-                                                                innovative,
-                                                                effective
-                                                                websites that
-                                                                capture your
-                                                                brand, improve
-                                                                your conversion
-                                                                rates, and
-                                                                maximize your
-                                                                revenue to help
-                                                                grow your
-                                                                business and
-                                                                achieve your
-                                                                goals.
+                                                               
                                                             </p>
                                                         </div>
                                                     </div>
@@ -225,9 +206,11 @@ const News = () => {
                 <div className="box_inner">
                     <div className="description_wrap" style={{ padding: "30px" }}>
                         <div className="news_popup_informations">
-                            <div className="image center">
+                                <h3>COMING SOON</h3>
+                            <div className="image modal-center">
                                 <img src="img/spotify.png" width="400" alt="" />
                                 <img src="img/apple_podcasts.png"  width="400" alt="" />
+                                <img src="img/twitter_spaces.png"  width="400" alt="" />
                             </div>
                             <br />
                             <div className="details" style={{marginBottom:"20px"}}>
